4b6b5332919f1b31235fea864d7ea4a7ecc581e1
iamgiddyaboutgit <justinwitherlypatterson@gmail.com>
2024-07-21 16:57:18 -0600

    Implement logic for read_seq_from_fasta generator.

--- global_align.py
+++ global_align.py
@@ -11,13 +11,6 @@ import sys
 import argparse
 from pathlib import Path
 
-# The cost for a gap just to exist.
-# This cost should be non-negative.
-# This cost can be incurred multiple times
-# if there are multiple runs of gaps in the
-# alignment.
-GAP_EXISTENCE_COST = 0
-
 def main():
     usage = "Perform optimal global alignment of two nucleotide \
 or amino acid sequences using the Needleman-Wunsch algorithm."
@@ -31,14 +24,41 @@ or amino acid sequences using the Needleman-Wunsch algorithm."
     parser.add_argument(
         "-s", 
         required=True,
-        help="file path to scoring matrix text file"
+        help="File path to a scoring matrix text file."
+    ) 
+
+    parser.add_argument(
+        "-i", 
+        required=True,
+        help="File path to a FASTA file containing two sequences to align."
+    ) 
+
+    parser.add_argument(
+        "-g", 
+        required=False,
+        default=0,
+        help="Cost for opening a run of gaps. Should be non-negative."
     ) 
 
     cmd_line_args = parser.parse_args()
 
     path_to_score_matrix_file = Path(cmd_line_args.s)
+    path_to_fasta_file = Path(cmd_line_args.i)
+    gap_existence_cost = int(cmd_line_args.g)
     
-    # Read in FASTA file.
+    # Read in descriptions and sequences from FASTA file.
+    counter = 0
+    for desc_and_seq in read_seq_from_fasta(fasta_path=path_to_fasta_file):
+        counter += 1
+        if counter == 1:
+            desc_1, seq_1 = desc_and_seq
+        elif counter == 2:
+            desc_2, seq_2 = desc_and_seq
+        else:
+            break
+   
+    print("seq_2")
+    print(seq_2)
     # Verify FASTA file is in the correct format.
     # Extract sequences.
     # Verify sequences are formatted correctly.
@@ -60,9 +80,24 @@ or amino acid sequences using the Needleman-Wunsch algorithm."
     # scoring matrix.  If there's a mismatch, then 
     # raise an exception.  In other words, the sequences
     # should only contain letters present in the scoring matrix.
-
+    scoring_mat_letters = scoring_mat.keys()
+    if not all([letter in scoring_mat_letters for letter in seq_1]):
+        raise RuntimeError("There were letters in seq_1 not present in scoring_mat.")
+    if not all([letter in scoring_mat_letters for letter in seq_2]):
+        raise RuntimeError("There were letters in seq_2 not present in scoring_mat.")
     # Perform the alignment, insert gaps, and compute the score.
-
+    alignment = align(
+        seq_1=seq_1,
+        seq_2=seq_2,
+        scoring_mat=scoring_mat,
+        gap_existence_cost=gap_existence_cost
+    )
+    print_alignment(
+        desc_1=desc_1,
+        desc_2=desc_2,
+        alignment=alignment
+    )
+   
     # Write the outputs to a file.
 
 
@@ -114,12 +149,161 @@ def read_scoring_mat(scoring_mat_path:Path) -> dict[dict]:
              
     return scoring_mat
 
+def read_seq_from_fasta(fasta_path:Path):
+    """Read in a FASTA file. 
+
+    Yields:
+        2-tuples where the 0th element is the description
+        and the 1st element is the sequence
+    
+    See: NCBI FASTA specification
+    https://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Web&PAGE_TYPE=BlastDocs&DOC_TYPE=BlastHelp
+    """
+    if not fasta_path.is_file():
+        raise FileNotFoundError("fasta_path does not point to a valid file.")
+    
+    
+    with fasta_path.open() as f:
+        desc_and_seq_complete = False
+        seq_list = []
+        line = f.readline()
+        line_stripped = line.strip()
+
+        if not line_stripped.startswith(">"):
+            raise RuntimeError("Invalid FASTA format. Expected the first line to start with '>'.")
+            
+        desc = line_stripped
+
+        for line in f:
+            line_stripped = line.strip()
+
+            if line_stripped.startswith(">"):
+                # We have reached a description
+                # other than the first one.
+                # We are ready to yield.
+                seq = "".join(seq_list)
+                if not (len(seq) > 0):
+                    raise RuntimeError("Empty sequence detected in FASTA.")
+                yield (desc, seq)
+                # Prepare for the next yield
+                # with the description we just
+                # found.
+                desc = line_stripped
+                # Clear seq_list for the next seq.
+                seq_list.clear()
+            elif len(line_stripped) > 0:
+                # Append the sequence on the line_stripped.
+                seq_list.append(line_stripped)
+
+        # We have reached the end of the file.
+        # We are ready to yield.
+        seq = "".join(seq_list)
+        if not (len(seq) > 0):
+            raise RuntimeError("Empty sequence detected in FASTA.")
+        
+        yield (desc, seq)
+
+        
+    # with fasta_path.open() as f:
+    #     # Prep for loop
+    #     desc = None
+    #     seq_list = []
+
+    #     # Get the first desc.
+    #     line = f.readline()
+    #     line_stripped = line.strip()
+
+    #     is_desc = line_stripped.startswith(">")
+    #     if not is_desc:
+    #         raise RuntimeError("Invalid FASTA format. Expected the first line to start with '>'.")
+        
+    #     desc = line_stripped
+  
+    #     for line in f:
+    #         line_stripped = line.strip()
+    #         is_desc = line_stripped.startswith(">")
+    #         is_seq = (not is_desc) and (line_stripped != "")
+            
+    #         if is_seq:
+    #             # Append what is there and then
+    #             # go to the next line to possibly
+    #             # append more.
+    #             seq_list.append(line_stripped)
+    #             continue
+    #         elif is_desc:
+    #             desc = line_stripped
+    #             seq = "".join(seq_list)
+    #             yield (desc, seq)
+
+    #     # We have consumed the entire FASTA file.        
+    #     seq = "".join(seq_list)
+    #     yield (desc, seq)
+        
+        ###########
+
+        #     is_seq = (not is_desc) and line_stripped.isalpha() 
+            
+        #     if is_seq:
+        #         # Append what is there and then
+        #         # go to the next line to possibly
+        #         # append more.
+        #         seq_list.append(line_stripped)
+        #         continue
+        #     elif is_desc:
+        #         desc = line_stripped
+
+        #     seq = "".join(seq_list)
+        #     if desc is not None and seq is not None:
+        #         yield (desc, seq)
+        #         desc = None
+        #         seq = None
+
+        # for line in f:
+        #     line_stripped = line.strip()
+        #     is_desc = line_stripped.startswith(">")
+        #     is_seq = (not is_desc) and line_stripped.isalpha() 
+            
+        #     if is_seq:
+        #         # Append what is there and then
+        #         # go to the next line to possibly
+        #         # append more.
+        #         seq_list.append(line_stripped)
+        #         continue
+        #     elif is_desc:
+        #         desc = line_stripped
+
+        #     seq = "".join(seq_list)
+        #     if desc is not None and seq is not None:
+        #         yield (desc, seq)
+        #         desc = None
+        #         seq = None
+
+
+    
+
+
 def align(
     seq_1:str, 
     seq_2:str, 
     scoring_mat:dict[dict], 
     gap_existence_cost:int
 ) -> tuple[str, str, str, int]:
+    """
+    Args:
+        gap_existence_cost: The cost for a gap just to exist.
+            This cost should be non-negative.
+            It can be incurred multiple times
+            if there are multiple runs of gaps in the
+            alignment.
+
+    Returns:
+        (
+            seq_1_aligned_out,
+            middle_part_out,
+            seq_2_aligned_out,
+            score
+        )
+    """
     m = len(seq_1)
     n = len(seq_2)
     dynamic_prog_num_rows = m + 1
@@ -358,6 +542,16 @@ def init_partial_dynamic_prog_matrix(
 
     mat[1][0] = -gap_existence_cost + scoring_mat[seq_1[0]]["-"]
     return mat
+
+def print_alignment(desc_1:str, desc_2:str, alignment:tuple[str, str, str, int]):
+    # TODO: Handle long alignments with proper line breaking.
+    print(desc_1)
+    print(desc_2)
+    print("")
+    print(alignment[0])
+    print(alignment[1])
+    print(alignment[2])
+    print(f"score={str(alignment[3])}")
 
 
 if __name__ == "__main__":
