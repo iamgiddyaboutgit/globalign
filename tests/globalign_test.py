251dcb8c15d89f7b645376f3511e7945ababe5d2
iamgiddyaboutgit <justinwitherlypatterson@gmail.com>
2025-06-02 11:50:23 -0600

    Parametrize test of func. check_symmetric

--- tests/globalign_test.py
+++ tests/globalign_test.py
@@ -3,43 +3,64 @@ import pytest
 
 from src.globalign import globalign as ga
 
-def test_check_symmetric():
-    assert ga.check_symmetric(
-        {
-            "a": {"a": 4, "b": 3},
-            "b": {"a": 3, "b": 4}
-        }
+@pytest.mark.parametrize(
+    argnames=("test_input", "expected"),
+    argvalues=(
+        (
+            {
+                "a": {"a": 4, "b": 3},
+                "b": {"a": 3, "b": 4}
+            },
+            True
+        ),
+        (
+            {
+                "a": {"a": 4, "b": 3, "c": 0},
+                "b": {"a": 3, "b": 4, "c": 7},
+                "c": {"a": 0, "b": 7, "c": 1}
+            },
+            True
+        ),
+        (
+            {
+                "a": {"a": 4, "b": 3, "c": 0},
+                "b": {"a": 3, "b": 4, "c": 7},
+                "c": {"a": 0, "b": 17, "c": 1}
+            },
+            False
+        ),
+        (
+            {
+                "a": {"a": 4, "b": 3, "c": 0},
+                "b": {"a": 3, "b": 4, "c": 7},
+                "d": {"a": 0, "b": 7, "c": 1}
+            },
+            False
+        )
     )
+)
+def test_check_symmetric_valid_input(test_input, expected):
+    assert ga.check_symmetric(test_input) == expected
 
-    assert ga.check_symmetric(
-        {
-            "a": {"a": 4, "b": 3, "c": 0},
-            "b": {"a": 3, "b": 4, "c": 7},
-            "c": {"a": 0, "b": 7, "c": 1}
-        }
+
+@pytest.mark.parametrize(
+    argnames=("test_input", "expected"),
+    argvalues=(
+        (
+            0,
+            AttributeError
+        ),
+        (
+            None,
+            AttributeError
+        ),
+        (
+            [[1, 4], [4, 1]],
+            AttributeError
+        ),
     )
+)
+def test_check_symmetric_invalid_input(test_input, expected):
+    with pytest.raises(expected):
+        ga.check_symmetric(test_input)
 
-    assert not ga.check_symmetric(
-        {
-            "a": {"a": 4, "b": 3, "c": 0},
-            "b": {"a": 3, "b": 4, "c": 7},
-            "c": {"a": 0, "b": 17, "c": 1}
-        }
-    )
-
-    assert not ga.check_symmetric(
-        {
-            "a": {"a": 4, "b": 3, "c": 0},
-            "b": {"a": 3, "b": 4, "c": 7},
-            "d": {"a": 0, "b": 7, "c": 1}
-        }
-    )
-
-    with pytest.raises(AttributeError):
-        ga.check_symmetric(0)
-
-    with pytest.raises(AttributeError):
-        ga.check_symmetric(None)
-
-    with pytest.raises(AttributeError):
-        ga.check_symmetric([[1, 4], [4, 1]])
